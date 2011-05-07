59e8d649ee99c9fe6f9aba3b7b2f0463cfb526eb
dan <danharibo@gmail.com>
2011-05-08 00:06:01 +0100

    * itoa -> custom version (dammit g++) in Util::toString

--- include/util.h
+++ include/util.h
@@ -26,9 +26,32 @@ public:
 
 	static std::string toString(int number)
 	{
-		char* buff = new char[100];
-		itoa(number, buff, 10);
-		return std::string( buff );
+		return itoa(number, 10);
+	}
+
+	/**
+	 * C++ version std::string style "itoa":
+	*/
+	static std::string itoa(int value, int base) {
+		enum { kMaxDigits = 35 };
+		std::string buf;
+		buf.reserve( kMaxDigits ); // Pre-allocate enough space.
+	
+		// check that the base if valid
+	
+		if (base < 2 || base > 16) return buf;
+		int quotient = value;
+		// Translating number to string with base:
+		do {
+			buf += "0123456789abcdef"[ std::abs( quotient % base ) ];
+			quotient /= base;
+		
+		} while ( quotient );
+
+		// Append the negative sign for base 10
+		if ( value < 0 && base == 10) buf += '-';
+		std::reverse( buf.begin(), buf.end() );
+		return buf;
 	}
 
 	// Causes Ambiguity errors.
