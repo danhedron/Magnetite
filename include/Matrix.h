b5ec82d6b3875cb8798018386f34a42b1b0972a5
dan <danharibo@gmail.com>
2011-06-24 00:29:20 +0800

    Improved matrix class.

--- include/Matrix.h
+++ include/Matrix.h
@@ -6,23 +6,10 @@
 
 class Matrix4 {
 public:
-	float matrix[16];
-	float* m00;
-	float* m01;
-	float* m02;
-	float* m03;
-	float* m10;
-	float* m11;
-	float* m12;
-	float* m13;
-	float* m20;
-	float* m21;
-	float* m22;
-	float* m23;
-	float* m30;
-	float* m31;
-	float* m32;
-	float* m33;
+	union {
+		float matrix[16];
+		float m[4][4];
+	};
 
 	Matrix4();
 	~Matrix4();
