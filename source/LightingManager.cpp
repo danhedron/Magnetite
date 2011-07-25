4248bae9b8fb9b258ad8ca4e0d62bdd0dee954fe
Daniel Evans <danharibo@gmail.com>
2011-07-25 18:14:37 +0100

    Adjacent chunks now update when a chunk changes

--- source/LightingManager.cpp
+++ source/LightingManager.cpp
@@ -42,5 +42,59 @@ void LightingManager::lightColumn( WorldChunk* chunk, size_t x, size_t z, LightI
 		{
 			chunk->setLightLevel( x, y, z, 0 );
 		}
+
+		//Check -X 
+		if( chunk->getBlockAt(x-1, y, z) )
+		{
+			directionFlags |= NEG_X;
+		}
+		else if( (directionFlags & NEG_X) == NEG_X )
+		{
+			// light shadow
+			lightUnderground( chunk, x - 1, y, z, NEG_X, WorldChunk::Sunlight & 1 );
+		}
+
+		//Check +X 
+		if( chunk->getBlockAt(x+1, y, z) )
+		{
+			directionFlags |= POS_X;
+		}
+		else if( (directionFlags & POS_X) == POS_X )
+		{
+			// light shadow
+			lightUnderground( chunk, x + 1, y, z, POS_X, WorldChunk::Sunlight & 1 );
+		}
+
+		//Check -Z
+		if( chunk->getBlockAt(x, y, z-1) )
+		{
+			directionFlags |= NEG_Z;
+		}
+		else if( (directionFlags & NEG_Z) == NEG_Z )
+		{
+			// light shadow
+			lightUnderground( chunk, x, y, z - 1, NEG_Z, WorldChunk::Sunlight & 1 );
+		}
+
+		//Check +Z 
+		if( chunk->getBlockAt(x, y, z+1) )
+		{
+			directionFlags |= POS_Z;
+		}
+		else if( (directionFlags & POS_Z) == POS_Z )
+		{
+			// light shadow
+			lightUnderground( chunk, x, y, z+1, POS_Z, WorldChunk::Sunlight & 1 );
+		}
+	}
+}
+
+void LightingManager::lightUnderground( WorldChunk* chunk, size_t x, size_t startY, size_t z, size_t dir, LightIndex light )
+{
+	size_t currentLight = light;
+	
+	for( short y = startY; y >= 0; y-- )
+	{
+		
 	}
 }
