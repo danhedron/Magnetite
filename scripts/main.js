24ae1c3c54232165367c376c43c9115d40c6be26
Daniel Evans <danharibo@gmail.com>
2012-02-16 07:21:57 +0000

    Meta API
    Added block types, more api to come.

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,10 @@ if( someBlock != undefined )
 {
 	console.log(someBlock.type);
 }
+
+console.log('Available Types: ');
+var types = meta.blocks.availableTypes;
+for( var x = 0; x < types.length; x += 1 )
+{
+	console.log( '\t' + types[x] );
+}
