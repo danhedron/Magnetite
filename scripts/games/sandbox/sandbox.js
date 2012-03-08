9d56e8eb3faaaa377c494273000e2950f75b82f8
Daniel Evans <danharibo@gmail.com>
2012-03-08 18:40:23 +0000

    Added smooth camera

--- scripts/games/sandbox/sandbox.js
+++ scripts/games/sandbox/sandbox.js
@@ -1,5 +1,15 @@
 Game.name = 'Sandbox';
 
+Number.prototype.sign = function() {
+  if(this > 0) {
+    return 1;
+  } else if (this < 0) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
 Game.tools = [
 	{
 		title: 'place',
@@ -46,7 +56,9 @@ Game.onSpawn = function( p )
 {
 	this.player = p;
 	this.player.setPosition( { x: 10, y: 130, z: 10 } );
-	this.player.moveDir = { x: 0, y: 0, z: 0 };
+	this.player.moveTarget = { x: 0, y: 0, z: 0 };
+	this.player.moveSpeed = { x: 0, y: 0, z: 0 };
+	this.player.movementSpeed = 1;
 }
 
 Game.changeBlock = function( v )
@@ -73,25 +85,25 @@ Game.keyDown = function(k)
 			break;
 		case 22:
 			// W
-			this.player.moveDir.z = -10;
+			this.player.moveTarget.z = -10;
 			break;
 		case 0:
 			// A
-			this.player.moveDir.x = 10;
+			this.player.moveTarget.x = 10;
 			break;
 		case 18:
 			// S
-			this.player.moveDir.z = 10;
+			this.player.moveTarget.z = 10;
 			break;
 		case 3:
 			// D
-			this.player.moveDir.x = -10;
+			this.player.moveTarget.x = -10;
 			break;
 		case 16:
-			this.player.moveDir.y = 10;
+			this.player.moveTarget.y = 10;
 			break;
 		case 4:
-			this.player.moveDir.y = -10;
+			this.player.moveTarget.y = -10;
 			break;
 	}
 }
@@ -102,23 +114,23 @@ Game.keyUp = function(k)
 	{
 		case 22:
 			// W
-			this.player.moveDir.z = 0;
+			this.player.moveTarget.z = 0;
 			break;
 		case 0:
 			// A
-			this.player.moveDir.x = -0;
+			this.player.moveTarget.x = -0;
 			break;
 		case 18:
 			// S
-			this.player.moveDir.z = -0;
+			this.player.moveTarget.z = -0;
 			break;
 		case 3:
 			// D
-			this.player.moveDir.x = 0;
+			this.player.moveTarget.x = 0;
 			break;
 		case 16:
 		case 4:
-			this.player.moveDir.y = 0;
+			this.player.moveTarget.y = 0;
 			break;
 	}
 }
@@ -135,9 +147,24 @@ Game.onAlt = function( player )
 
 Game.think = function( dt )
 {
-	if( this.player.moveDir.x != 0 || this.player.moveDir.y != 0 || this.player.moveDir.z != 0 )
+	var accel = 5.0;
+	var decel = 10.0;
+	for( var d in this.player.moveTarget )
 	{
-		this.player.move( this.player.moveDir.z * dt,  this.player.moveDir.y * dt, this.player.moveDir.x * dt );
+		if( Math.abs(this.player.moveTarget[d]) - Math.abs(this.player.moveSpeed[d]) > 0.0001 )
+		{
+			var s = this.player.moveTarget[d].sign();
+			this.player.moveSpeed[d] = s * Math.min( Math.abs(this.player.moveTarget[d]), Math.abs(this.player.moveSpeed[d]) + accel * dt);
+		}
+		else if( Math.abs(this.player.moveSpeed[d]) - Math.abs(this.player.moveTarget[d]) > 0.0001 )
+		{
+			var s = this.player.moveSpeed[d].sign();
+			this.player.moveSpeed[d] = s * Math.max( Math.abs(this.player.moveTarget[d]), Math.abs(this.player.moveSpeed[d]) - decel * dt);
+		}
+	}
+	if( this.player.moveSpeed.x != 0 || this.player.moveSpeed.y != 0 || this.player.moveSpeed.z != 0 )
+	{
+		this.player.move( this.player.moveSpeed.z * this.player.movementSpeed * dt, this.player.moveSpeed.y * this.player.movementSpeed * dt, this.player.moveSpeed.x * this.player.movementSpeed * dt );
 	}
 }
 
