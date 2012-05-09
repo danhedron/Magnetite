e64726db460af3e1fa256ac599d57c22d737fb16
Daniel Evans <danharibo@gmail.com>
2012-05-09 11:10:00 +0100

    Profiler

---
+++ include/Profiler.h
@@ -0,0 +1,32 @@
+#ifndef _PROFILER_H_
+#define _PROFILER_H_
+#include <map>
+#include <string>
+
+namespace Perf {
+	
+	struct ProfileEntry
+	{
+		std::string name;
+		uint32_t total;
+		uint32_t currentStart;
+		uint32_t latest;
+	};
+	
+	typedef std::map<std::string,ProfileEntry> ProfilerEvents;
+	
+	class Profiler
+	{
+		ProfilerEvents mEvents;
+		
+	public:
+		void begin( const std::string& section );
+		
+		void end( const std::string& section );
+		
+		static Profiler& get();
+	};
+
+}
+
+#endif
---
+++ source/Profiler.cpp
@@ -0,0 +1,51 @@
+#include "Profiler.h"
+#include <util.h>
+#include <sys/time.h>
+
+namespace Perf 
+{
+	
+	Profiler& Profiler::get()
+	{
+		static Profiler prof;
+		return prof;
+	}
+	
+	void Profiler::begin( const std::string& section )
+	{
+		ProfilerEvents::iterator it = mEvents.find( section );
+		if( it != mEvents.end() )
+		{
+			timeval time;
+			gettimeofday(&time, NULL);
+			long millis = (time.tv_sec * 1000) + (time.tv_usec / 1000);
+			it->second.currentStart = millis;
+			return;
+		}
+		
+		ProfileEntry ent;
+		timeval time;
+		gettimeofday(&time, NULL);
+		long millis = (time.tv_sec * 1000) + (time.tv_usec / 1000);
+		ent.currentStart = millis;
+		ent.latest = 0;
+		ent.name = section;
+		ent.total = 0;
+		
+		mEvents.insert( ProfilerEvents::value_type( section, ent ) );
+	}
+	
+	void Profiler::end( const std::string& section )
+	{
+		ProfilerEvents::iterator it = mEvents.find( section );
+		if( it != mEvents.end() )
+		{
+			timeval time;
+			gettimeofday(&time, NULL);
+			long millis = (time.tv_sec * 1000) + (time.tv_usec / 1000);
+			it->second.latest = millis - it->second.currentStart;
+			it->second.total += it->second.latest;
+			Util::log("Exiting " + section + " (" + Util::toString(it->second.latest) + ")");
+		}
+	}
+};
